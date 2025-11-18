631fda572a6c3161c0fe1cbeec9b193688d19f33
STORMGAMER0 <eobi816@gmail.com>
2025-11-18 23:50:42 +0100

    added get tasks endpoint with all related filters, pagination and services

--- app/api/v1/endpoints/tasks.py
+++ app/api/v1/endpoints/tasks.py
@@ -1,7 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from typing import Optional
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.database import get_db
+from models.task import TaskPriority
 from models.user import User
 from schemas.task import TaskCreate, TaskUpdate, TaskStatus, TaskResponse
 from app.api.dependencies import get_current_user
@@ -15,5 +18,29 @@ async def create_task(task: TaskCreate, current_user: User= Depends(get_current_
     new_task = await TaskService.create_task(db,task,current_user.id)
     return new_task
 
+@task_router.get("/", response_model=list[TaskResponse], status_code=status.HTTP_200_OK)
+async def get_tasks(status:Optional[TaskStatus] = Query(None, description="filter by status"),
+                    priority: Optional[TaskPriority] = Query(None, description="filter by priority"),
+                    search: Optional[str] = Query(None, description="search in title and description"),
+                    assigned_to: Optional[UUID] = Query(None, description="filter by assigned to"),
+                    sort_by : str = Query("created_at", description="Sort field"),
+                    order: str = Query("desc", description="sort order: asc or desc"),
+                    page: int = Query(1, ge=1, description= "page number"),
+                    limit: int = Query(10, ge=1, le=100, description="items per page"),
+                    current_user: User = Depends (get_current_user),
+                    db: AsyncSession = Depends(get_db)
+                    ):
 
+    tasks = await TaskService.get_tasks(db=db,
+        current_user=current_user,
+        status=status,
+        priority=priority,
+        search=search,
+        assigned_to=assigned_to,
+        sort_by=sort_by,
+        order=order,
+        page=page,
+        limit=limit)
+
+    return tasks
 
--- services/task.py
+++ services/task.py
@@ -1,43 +1,109 @@
 from uuid import UUID
+from typing import Optional
+
 from fastapi import HTTPException, status
+from sqlalchemy import select, func, or_
+from sqlalchemy.exc import IntegrityError
 
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from models.task import Task, TaskStatus, TaskPriority
 from schemas.task import TaskCreate, TaskUpdate
 from core.logger import get_logger
+from models.user import User, UserRole
 
 logger = get_logger(__name__)
 
+
 class TaskService:
     @staticmethod
-
     async def create_task(db: AsyncSession, task: TaskCreate, created_by: UUID):
-        from models.task import Task,TaskStatus
+        from models.task import Task, TaskStatus
+        new_task = Task(
+            title=task.title,
+            description=task.description,
+            priority=task.priority,
+            due_date=task.due_date,
+            assigned_to=task.assigned_to,
+            created_by=created_by
+        )
+
+        db.add(new_task)
         try:
-            new_task = Task(
-                title = task.title,
-                description = task.description,
-                priority = task.priority,
-                due_date = task.due_date,
-                assigned_to = task.assigned_to,
-                created_by = created_by
-            )
-            db.add(new_task)
             await db.commit()
             await db.refresh(new_task)
-
             logger.info(f"Task created with id {new_task.id}")
             return new_task
 
-        except Exception as e:
+        except IntegrityError as e:
             await db.rollback()
-            logger.exception(f"failed to create task: {e}")
+            if "foreign key" in str(e).lower():
+                raise HTTPException(
+                    status_code=status.HTTP_404_NOT_FOUND,
+                    detail="Assigned user not found"
+                )
             raise HTTPException(
-                status_code= status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail= "failed to create user"
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid task data"
             )
 
+    @staticmethod
+    async def get_tasks(db: AsyncSession, current_user: User, status: Optional[TaskStatus] = None,
+                        priority: Optional[TaskPriority] = None, search: Optional[str] = None,
+                        assigned_to: Optional[UUID] = None, sort_by: str = "created_at",
+                        order: str = "desc", page: int = 1, limit: int = 10 ):
+
+        query = select(Task)
+
+        if current_user.role == UserRole.MEMBER:
+            query = query.where(
+                or_(
+                    Task.created_by == current_user.id,
+                    Task.assigned_to == current_user.id
+                )
+            )
+        if status:
+            query = query.where(Task.status == status)
+
+        if priority:
+            query = query.where(Task.priority == priority)
+
+        if search:
+            search_pattern = f"%{search}%"
+            query = query.where(
+                or_(
+                    Task.title.ilike(search_pattern),
+                    Task.description.ilike(search_pattern)
+                )
+            )
+
+        if assigned_to:
+            query = query.where(Task.assigned_to == assigned_to)
+
+        if order == "asc":
+            query = query.order_by(getattr(Task, sort_by).asc())
+        else:
+            query = query.order_by(getattr(Task, sort_by).desc())
 
 
+        count_query = select(func.count()).select_from(query.subquery())
+        total_result = await db.execute(count_query)
+        total_count = total_result.scalar()
 
+        offset = (page - 1) * limit
+        query = query.offset(offset).limit(limit)
 
+        result = await db.execute(query)
+        tasks = result.scalars().all()
+
+        total_pages = (total_count + limit - 1) // limit
+
+        logger.info(f"user{current_user.id} fetched tasks (page{page}")
+
+        return {
+            "tasks": tasks,
+            "total": total_count,
+            "page": page,
+            "limit": limit,
+            "total_pages": total_pages
+        }
